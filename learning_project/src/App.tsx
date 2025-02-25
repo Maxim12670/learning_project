14a4e78d504206725149558a89187a1b7546268c
maxim <Akaikinmax@mail.ru>
2025-02-25 15:49:59 +0700

    main branch

--- learning_project/src/App.tsx
+++ learning_project/src/App.tsx
@@ -1,25 +1,7 @@
-import { useMemo, useState } from "react";
-
-const sum = (n: number) => {
-  console.log("i work");
-  return n + n;
-};
-
 function App() {
-  const [num, setNum] = useState<number>(1);
-  const [isGreen, setIsGreen] = useState<boolean>(false);
-  const result = useMemo(() => sum(num), [num]);
-
   return (
     <div>
-      <h1
-        onClick={() => setIsGreen(!isGreen)}
-        style={{ color: isGreen ? "green" : "red" }}
-      >
-        yyyyy
-      </h1>
-      <h2>Sum {result}</h2>
-      <button onClick={() => setNum(num + 1)}>➕</button>
+      <h2>main branch</h2>
     </div>
   );
 }
